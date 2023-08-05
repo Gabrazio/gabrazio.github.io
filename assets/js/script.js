ca097eda5704472e56f5a9c5ad5a65ff2e79f523
gabrazio <gabrielebuonocore.gb@gmail.com>
2023-08-05 17:38:17 +0200

    -

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    document.querySelector(".telegram").addEventListener('click', (event) => {
+        window.location = "https://t.me/Gabrazio";
+    });
+
+    document.querySelector(".github").addEventListener('click', (event) => {
+        window.location = "https://github.com/gabrazio";
+    });
+}
