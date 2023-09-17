fb1d2eac3a2963dc3918fc500a4ce849058b3a26
gabrazio <gabrielebuonocore.gb@gmail.com>
2023-09-17 18:09:44 +0200

    -

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,27 @@
 window.onload = () => {
-    document.querySelector(".telegram").addEventListener('click', (event) => {
-        window.location = "https://t.me/Gabrazio";
+    const instagram = document.getElementById("instagram");
+    const telegram = document.getElementById("telegram");
+    const discord = document.getElementById("discord");
+    const spotify = document.getElementById("spotify");
+    const steam = document.getElementById("steam");
+
+    instagram.addEventListener("click", (event) => {
+        window.open("https://www.instagram.com/gabrazio");
     });
 
-    document.querySelector(".github").addEventListener('click', (event) => {
-        window.location = "https://github.com/gabrazio";
+    telegram.addEventListener("click", (event) => {
+        window.open("https://t.me/Gabrazio");
+    });
+
+    discord.addEventListener("click", (event) => {
+        window.open("https://discord.gg/9c59zuEuM");
+    });
+
+    spotify.addEventListener("click", (event) => {
+        window.open("https://open.spotify.com/user/315tjw2pqcnsvba6b3ggadalmzme");
+    });
+
+    steam.addEventListener("click", (event) => {
+        window.open("https://steamcommunity.com/id/Gabrazio");
     });
 }
