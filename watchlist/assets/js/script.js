4530e2f2dc5fe9fff539953490f7eb3a7a009f06
gabrazio <gabrielebuonocore.gb@gmail.com>
2023-10-25 17:14:40 +0200

    aggiunto "The Boys" alla lista delle serie

--- watchlist/assets/js/script.js
+++ watchlist/assets/js/script.js
@@ -24,7 +24,7 @@ window.onload = () => {
     }
 
     var anime = ["Sword Art Online", "Another", "Mirai nikki - Future Diary", "La forma della voce", "Your Name.", "Hotarubi no mori e", "Wolf Children - Ame e Yuki i bambini lupo", "Death Parade", "One-Punch Man", "Noragami", "Death Note", "Weathering with You", "Voglio mangiare il tuo pancreas", "Il castello errante di Howl"];
-    var tv_series = ["How I Met Your Mother", "The Big Bang Theory", "Breaking Bad", "Stranger Things", "The Umbrella Academy", "BoJack Horseman", "The End of the F***ing World", "Sex Education", "La casa di carta", "Malcolm (Malcolm in the Middle)", "The Office", "Riverdale", "Mr. Robot", "Squid Game", "Atypical", "Alice in Borderland", "Strappare lungo i bordi", "Arcane", "Chernobyl", "Superstore", "Tredici"];
+    var tv_series = ["The Boys", "How I Met Your Mother", "The Big Bang Theory", "Breaking Bad", "Stranger Things", "The Umbrella Academy", "BoJack Horseman", "The End of the F***ing World", "Sex Education", "La casa di carta", "Malcolm (Malcolm in the Middle)", "The Office", "Riverdale", "Mr. Robot", "Squid Game", "Atypical", "Alice in Borderland", "Strappare lungo i bordi", "Arcane", "Chernobyl", "Superstore", "Tredici"];
 
     const tv_series_sorted = tv_series.sort();
     const anime_sorted = anime.sort();
