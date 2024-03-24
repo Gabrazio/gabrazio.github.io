0db3b53156f48e404d8a22b4d1f5495d56dc707d
Gabrazio <gabrielebuonocore.gb@gmail.com>
2024-03-24 19:20:22 +0100

    -

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,6 @@ window.onload = () => {
     }
 
     let img = document.getElementById("img");
-    let filename = ["1.jpg", "2.jpg", "3.jpg", "5.jpg", "6.jpg", "8.jpg"];
+    let filename = ["1.jpg", "2.jpg", "3.jpg", "5.jpg", "6.jpg", "8.jpg", "9.jpg"];
     img.style.backgroundImage = "url(assets/img/" + filename[Math.round(Math.floor(Math.random() * (filename.length)))];
 }
