e28c01c49b39e8ccbe04713c4e8150aa39d41f16
Gabrazio <gabrielebuonocore.gb@gmail.com>
2024-03-26 15:37:16 +0100

    -

--- free/assets/js/script.js
+++ free/assets/js/script.js
@@ -1,5 +1,7 @@
 window.onload = () => {
-    fetch("https://www.gamerpower.com/api/giveaways?type=game").catch((res) => {
-        console.log(res);
+    fetch("https://www.gamerpower.com/api/giveaways?type=game").then((res) => {
+        return res.json();
+    }).then((data) => {
+        console.log(data);
     });
 }
