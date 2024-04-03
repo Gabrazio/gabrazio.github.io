f031992b4ddbad698136e8a86feaa081021e1b08
Gabrazio <gabrielebuonocore.gb@gmail.com>
2024-04-03 11:40:12 +0200

    -

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 window.onload = () => {
     let img = document.getElementById("img");
     let filename = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg"];
-    img.style.backgroundImage = "url(assets/img/" + filename[Math.round(Math.floor(Math.random() * (filename.length)))];
+    img.style.backgroundImage = "url(assets/img/1.jpg";
+    var n = 0;
+
+    setInterval(() => {
+        n = (n % filename.length) + 1; 
+        img.style.backgroundImage = "url(assets/img/" + n + ".jpg";
+    }, 5000);
 }
