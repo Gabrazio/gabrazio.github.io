fad02712b5b0204146d5f24e3bf950a5e0643256
Gabrazio <80647580+Gabrazio@users.noreply.github.com>
2024-07-22 11:42:50 +0200

    -

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,28 @@
 window.onload = () => {
     var N = 12;
     var imgNumber = Math.floor(Math.random() * N) + 1;
-    console.log(imgNumber);
-
     var img = document.getElementById("meme");
     img.style.backgroundImage = "url(assets/img/" + imgNumber + ".jpg)";
+
+    var homeButton = document.getElementById("home");
+    var aboutButton = document.getElementById("about");
+    var blogButton = document.getElementById("blog");
+    var socialButton = document.getElementById("social");
+    var projectsButton = document.getElementById("projects");
+
+    aboutButton.onclick = () => {
+        alert("COMING SOON");
+    }
+
+    blogButton.onclick = () => {
+        alert("COMING SOON");
+    }
+
+    socialButton.onclick = () => {
+        alert("COMING SOON");
+    }
+
+    projectsButton.onclick = () => {
+        alert("COMING SOON");
+    }
 }
