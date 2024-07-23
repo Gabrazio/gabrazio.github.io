d8415979518cf40c6cf791158534838366d86cc3
Gabrazio <80647580+Gabrazio@users.noreply.github.com>
2024-07-23 17:50:12 +0200

    -

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,30 +3,4 @@ window.onload = () => {
     var imgNumber = Math.floor(Math.random() * N) + 1;
     var img = document.getElementById("meme");
     img.style.backgroundImage = "url(assets/img/" + imgNumber + ".jpg)";
-
-    var homeButton = document.getElementById("home");
-    var aboutButton = document.getElementById("about");
-    var blogButton = document.getElementById("blog");
-    var socialButton = document.getElementById("social");
-    var projectsButton = document.getElementById("projects");
-
-    homeButton.onclick = () => {
-        window.location.href = "index.html";
-    }
-
-    aboutButton.onclick = () => {
-        alert("COMING SOON");
-    }
-
-    blogButton.onclick = () => {
-        alert("COMING SOON");
-    }
-
-    socialButton.onclick = () => {
-        alert("COMING SOON");
-    }
-
-    projectsButton.onclick = () => {
-        alert("COMING SOON");
-    }
 }
