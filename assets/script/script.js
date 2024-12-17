3d35c7ec6c865402602f5ad18955a70d39fd45ea
Gabrazio <80647580+Gabrazio@users.noreply.github.com>
2024-12-17 10:57:11 +0100

    -

--- assets/script/script.js
+++
@@ -1,6 +0,0 @@
-window.onload = () => {
-    var N = 12;
-    var imgNumber = Math.floor(Math.random() * N) + 1;
-    var img = document.getElementById("meme");
-    img.style.backgroundImage = "url(assets/img/" + imgNumber + ".jpg)";
-}
